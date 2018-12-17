90b060433b6890669f7d78b8fca81d218e1a1f13
Neil Hoskins <da-poodle@protonmail.com>
2018-12-18 09:20:06 +1030

    updated versions

--- pack.pl
+++ pack.pl
@@ -1,7 +1,7 @@
 name(fld).
-version('0.0.2').
-title('Flexible Get/Set access term args that is refactor safe.').
-keywords([field,term,refactor]).
+version('0.1.0').
+title('Easy assess to term args when loading from ODBC or CSV.').
+keywords([field,term,refactor,odbc,csv]).
 author('Neil Hoskins', 'https://github.com/da-poodle').
 packager('Neil Hoskins', 'https://github.com/da-poodle').
 maintainer('Neil Hoskins', 'https://github.com/da-poodle').
