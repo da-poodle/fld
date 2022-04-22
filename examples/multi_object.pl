ee5eb94e46205cee668c8788ec2f14b7b5f11048
Neil Hoskins <23515561+da-poodle@users.noreply.github.com>
2022-04-22 11:05:58 +0930

    Update multi_object.pl

--- examples/multi_object.pl
+++ examples/multi_object.pl
@@ -4,18 +4,18 @@
 :- fld_object(bus, [company, model, registration, expires, year_of_make]).
 
 registration_valid(RoadUser) :-
-  flds([registration(Rego), expires(Expires)], RoadUser),
+  fld RoadUser:[registration(Rego), expires(Expires)],
   get_time(Time),
   (   Time < Expires -> true
   ; format('Rego ~p is expired~n', Rego)).
 
 
 test :-
-    fld_template(car, C),
-    flds([registration('ABC-123'), expires(1544947900.570324)], C),
+    fld C:car,
+    fld C:[registration('ABC-123'), expires(1544947900.570324)],
     registration_valid(C).
 
 test :-
-    fld_template(bus, B),
-    flds([registration('DA-BUS01'), expires(1544947900.570324)], B),
+    fld B:bus,
+    fld B:[registration('DA-BUS01'), expires(1544947900.570324)],
     registration_valid(B).
